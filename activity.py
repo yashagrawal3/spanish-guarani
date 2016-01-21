ac896b7c49051130d140ef9d53dcc99290a5eba3
Cristian García <cristian99garcia@gmail.com>
2016-01-20 21:20:24 -0300

    Porting to Gtk3

--- activity.py
+++ activity.py
@@ -13,132 +13,136 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-import gtk
-import pygtk
-pygtk.require('2.0')
-import logging
 
+import os
 from gettext import gettext as _
 from ConfigParser import SafeConfigParser
 
-class Base():
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import GdkPixbuf
 
-    def __init__(self):
+from sugar3.activity.activity import Activity
+from sugar3.activity.widgets import StopButton
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.graphics.toolbarbox import ToolbarBox
 
-        # label with the text, make the string translatable
-        ven = gtk.Window()
-        win = gtk.VBox()
-        eb = gtk.EventBox()     
-        eb.add(win)
-        eb.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color('White'))
-        title = gtk.Image()
-        achehety = gtk.Image()
-        texto = gtk.Entry()
-        traducido=gtk.TextView()
-        traducido.set_editable(False)
-        traducido.set_wrap_mode(gtk.WRAP_WORD)
-        dic = gtk.TextView()
-        textbuffer = dic.get_buffer()
-        dic.set_wrap_mode(gtk.WRAP_WORD)
-        dic.set_editable(False)
-        
-        hbox3=gtk.HButtonBox()  
-        hbox3.set_layout(gtk.BUTTONBOX_CENTER)
-        
+
+class Hablando_Guarani(Activity):
+
+    def __init__(self, handle):
+        Activity.__init__(self, handle)
+
+        self._setup_toolbarbox()
+
+        eb = Gtk.EventBox()
+        eb.modify_bg(Gtk.StateType.NORMAL, Gdk.Color.parse("white")[1])
+        self.set_canvas(eb)
+
+        vbox = Gtk.VBox()
+        eb.add(vbox)
+
+        title = Gtk.Image.new_from_pixbuf(self._scale_pixbuf("images/logo.jpg", 400, 100))
+        vbox.pack_start(title, False, False, 0)
+
+        self.entry = Gtk.Entry()
+        self.entry.connect("activate", self.translate_cb)
+        self.entry.connect("backspace", self.backspace_cb)
+        vbox.pack_start(self.entry, False, False, 0)
+
+        scroll = Gtk.ScrolledWindow()
+        vbox.pack_start(scroll, True, True, 0)
+
+        self.textview = Gtk.TextView()
+        self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        scroll.add(self.textview)
+
+        buttonbox = Gtk.HButtonBox()
+        buttonbox.set_layout(Gtk.ButtonBoxStyle.CENTER)
+        vbox.pack_start(buttonbox, False, False, 0)
+
         parser = SafeConfigParser()
-        parser.read('config.ini')   
-        bo1=gtk.Button(parser.get('dic','A'))
-        bo1.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color('#FCB947'))
-        bo2=gtk.Button(parser.get('dic','E'))
-        bo3=gtk.Button(parser.get('dic','I'))
-        bo3.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color('#FCB947'))
-        bo4=gtk.Button(parser.get('dic','O'))
-        bo5=gtk.Button(parser.get('dic','U'))
-        bo5.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color('#FCB947'))
-        bo6=gtk.Button(parser.get('dic','Y'))   
-        bo7=gtk.Button(parser.get('dic','G'))
-        bo7.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color('#FCB947'))
+        parser.read("config.ini")
 
-        pixbuf = gtk.gdk.pixbuf_new_from_file('images/logo.jpg')
-        scaled_pixbuf = pixbuf.scale_simple(400,100,gtk.gdk.INTERP_BILINEAR)
-        title.set_from_pixbuf(scaled_pixbuf)
-        pixbuf = gtk.gdk.pixbuf_new_from_file('images/achegety.jpg')
-        scaled_pixbuf = pixbuf.scale_simple(600,200,gtk.gdk.INTERP_BILINEAR)
-        achehety.set_from_pixbuf(scaled_pixbuf)
-        #connect
+        for data in [("dic", "A"), ("dic", "E"), ("dic", "I"), ("dic", "O"), ("dic", "U"), ("dic", "Y"), ("dic", "G")]:
+            button = Gtk.Button(parser.get(*data))
+            button.modify_bg(Gtk.StateType.NORMAL, Gdk.Color.parse('#FCB947')[1])
+            button.connect("clicked", self.__add, data[1])
+            buttonbox.add(button)
 
-        bo1.connect('clicked', self.__agregar__, texto, 'A')
-        bo2.connect('clicked', self.__agregar__, texto, 'E')
-        bo3.connect('clicked', self.__agregar__, texto, 'I')
-        bo4.connect('clicked', self.__agregar__, texto, 'O')
-        bo5.connect('clicked', self.__agregar__, texto, 'U')
-        bo6.connect('clicked', self.__agregar__, texto, 'Y')
-        bo7.connect('clicked', self.__agregar__, texto, 'G')
+        achegety = Gtk.Image.new_from_pixbuf(self._scale_pixbuf("images/achegety.jpg", 600, 200))
+        vbox.pack_start(achegety, False, False, 0)
+
+        scroll = Gtk.ScrolledWindow()
+        scroll.set_border_width(10)
+        scroll.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        vbox.pack_start(scroll, True, True, 0)
+
+        textview = Gtk.TextView()
+        textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        textview.set_editable(False)
+        scroll.add(textview)
+
+        self.buffer = textview.get_buffer()
+
+        parser = SafeConfigParser()
+        parser.read('config.ini')
+
         #Cargando archivo .txt
-        infile = open("lang/guarani/dic.txt", "r")
-        if infile:
-            string = infile.read()      
+        path = "lang/guarani/dic.txt"
+        if os.path.exists(path):
+            infile = open("lang/guarani/dic.txt", "r")
+            string = infile.read()
             infile.close()
-            textbuffer.set_text(string)
-    
-     
-        
-        hbox2 = gtk.HBox()
 
-        # Conexion de botones
-        texto.connect("activate", self.traducir_cb, traducido)
-        texto.connect("backspace", self.__backspace_cb, traducido)
+            self.buffer.set_text(string)
 
-        # creando scrolled
-        scrolled_window = gtk.ScrolledWindow()
-        scrolled_window.set_border_width(10)
-        scrolled_window.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_ALWAYS)
-        
-        scrolled_window.add_with_viewport(dic)
-        #Disenho de ventana
-        ven.add(eb)
-        win.add(title)
-        win.add(hbox3)
-        hbox3.add(bo1)
-        hbox3.add(bo2)
-        hbox3.add(bo3)
-        hbox3.add(bo4)
-        hbox3.add(bo5)
-        hbox3.add(bo6)
-        hbox3.add(bo7)
-        win.add(texto)
-        win.add(traducido)
-        win.add(hbox2)
-        win.add(achehety)
-        win.add(scrolled_window)
-     
-        ven.show_all()
+        self.show_all()
 
-    def __agregar__(self, bo1, texto=None, Data=None):
+    def _setup_toolbarbox(self):
+        toolbarbox = ToolbarBox()
+        self.set_toolbar_box(toolbarbox)
+
+        toolbarbox.toolbar.insert(ActivityToolbarButton(self), -1)
+        toolbarbox.toolbar.insert(StopButton(self), -1)
+
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_expand(True)
+        toolbarbox.toolbar.insert(separator, 1)
+
+        toolbarbox.show_all()
+        toolbarbox.toolbar.show_all()
+
+    def _scale_pixbuf(self, path, width, height):
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file('images/logo.jpg')
+        return pixbuf.scale_simple(width, height, GdkPixbuf.InterpType.BILINEAR)
+
+    def __add(self, button, data):
         parser = SafeConfigParser()
-        parser.read('config.ini')       
-        texto.set_text(texto.get_text()+parser.get('dic',Data))
-        
-    def traducir_cb(self, texto, traducido=None):
-        entry = texto.get_text()+' = '
-        cargar = traducido.get_buffer()
+        parser.read('config.ini')
+        self.entry.set_text(self.entry.get_text() + parser.get('dic', data))
+
+    def translate_cb(self, entry):
+        text = entry.get_text() + ' = '
+        buffer = self.textview.get_buffer()
         infile = "lang/guarani/dic.txt"
+
+        found = False
+
         with open(infile, 'r') as f:
             for line in f:
-                if line.lstrip().startswith(entry.capitalize()):
+                if line.lstrip().startswith(text.capitalize()):
                     line = line.rstrip()
-                    cargar.set_text(line) 
+                    buffer.set_text(line)
+
+                    found = True
                     break
-            
-                if entry != line:
-                    cargar.set_text('No se ha encontrado coincidencia')
-                
-    def __backspace_cb(self, texto, traducido=None):
-        cargar = traducido.get_buffer()        
-        cargar.set_text('')
 
-    def main(self):
-       gtk.main()
+        if not found:
+            buffer.set_text('No se ha encontrado coincidencia')
 
-base = Base()
-base.main()
+    def backspace_cb(self, entry):
+        buffer = self.textview.get_buffer()
+        buffer.set_text('')
+
